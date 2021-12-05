fc8cd1bea33a59ccfbdc2e8aacc746d7c1555238
airtonhjr <63892337+airtonhjr@users.noreply.github.com>
2021-12-05 02:26:27 -0300

    Project Reorganization

---
+++ src/CAS_AudioPlayer.dpr
@@ -0,0 +1,17 @@
+program CAS_AudioPlayer;
+
+uses
+  Vcl.Forms,
+  MainForm in 'MainForm.pas',
+  AsioList in '..\deps\asio\asiolist.pas',
+  Asio     in '..\deps\asio\asio.pas',
+  OpenASIO in '..\deps\asio\openasio.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TPlayerGUI, PlayerGUI);
+  Application.Run;
+end.
