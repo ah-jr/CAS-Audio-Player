35fb5539bfae23a0abad4c7c4b86ca9f11c4a467
airtonhjr <63892337+airtonhjr@users.noreply.github.com>
2021-12-11 20:45:52 -0300

    Separating CasEngine files

---
+++ deps/CasAudioEngine/CasEngine.pas
@@ -0,0 +1,22 @@
+unit CasEngine;
+
+interface
+
+uses
+  Winapi.Windows,
+  Winapi.Messages;
+
+type
+  TCasEngine = class
+
+  private
+    strText : String;
+  public
+
+  end;
+
+implementation
+
+end.
+
+
--- src/CAS_AudioPlayer.dpr
+++ src/CAS_AudioPlayer.dpr
@@ -2,10 +2,11 @@ program CAS_AudioPlayer;
 
 uses
   Vcl.Forms,
-  MainForm in 'MainForm.pas',
-  AsioList in '..\deps\asio\asiolist.pas',
-  Asio     in '..\deps\asio\asio.pas',
-  OpenASIO in '..\deps\asio\openasio.pas';
+  MainForm  in 'MainForm.pas',
+  AsioList  in '..\deps\asio\asiolist.pas',
+  Asio      in '..\deps\asio\asio.pas',
+  OpenASIO  in '..\deps\asio\openasio.pas',
+  CasEngine in '..\deps\CasAudioEngine\CasEngine.pas';
 
 {$R *.res}
 
